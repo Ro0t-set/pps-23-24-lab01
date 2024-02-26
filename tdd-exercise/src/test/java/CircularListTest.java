96831ecfd9b66ec727614e4fe97d380b29f5de2e
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2024-02-26 11:11:52 +0100

    ex 2 -- isInitialEmpty test pass

--- tdd-exercise/src/test/java/CircularListTest.java
+++ tdd-exercise/src/test/java/CircularListTest.java
@@ -1,17 +1,30 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import tdd.CicularListImpl;
+import tdd.CircularList;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList circularList;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+
+
+    @BeforeEach
+    void circularListCreation(){
+        circularList = new CicularListImpl();
+    }
+
+
+    @Test
+    void isInitialEmpty(){
+        assertTrue(circularList.isEmpty());
     }
 
 }
