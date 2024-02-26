bbe1442c2cb456971b70c634cc67f494de366924
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2024-02-26 19:11:09 +0100

    ex 2 -- finish

--- tdd-exercise/src/main/java/tdd/CicularListImpl.java
+++ tdd-exercise/src/main/java/tdd/CicularListImpl.java
@@ -7,6 +7,24 @@ import java.util.Optional;
 public class CicularListImpl implements CircularList  {
 
     private List<Integer> list = new ArrayList<Integer>();
+    private int index = - 1;
+
+    private Optional<Integer> indexUpdateAndGetListElement(){
+
+        if (this.isEmpty()){
+            index = -1;
+            return Optional.empty();
+        }
+        
+        if (index >= list.size()){
+            index = 0;
+        }else if(index < 0){
+            index = list.size() - 1;
+        }
+        return Optional.of(list.get(index));
+        
+    }
+
 
     @Override
     public void add(int element) {
@@ -25,20 +43,19 @@ public class CicularListImpl implements CircularList  {
 
     @Override
     public Optional<Integer> next() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'next'");
+        index++;
+        return indexUpdateAndGetListElement();
     }
 
     @Override
     public Optional<Integer> previous() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'previous'");
+        index--;
+        return indexUpdateAndGetListElement();
     }
 
     @Override
     public void reset() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'reset'");
+        list.clear();
     }
 
 }
--- tdd-exercise/src/test/java/CircularListTest.java
+++ tdd-exercise/src/test/java/CircularListTest.java
@@ -3,6 +3,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -35,10 +37,94 @@ public class CircularListTest {
     @Test 
     void addElement(){
         circularList.add(0);
-        assertEquals(1, circularList.size());
-        assertFalse(circularList.isEmpty());
+        assertAll(
+            ()->assertEquals(1, circularList.size()),
+            ()->assertFalse(circularList.isEmpty())
+        );
 
     }
+
+    @Test
+    void addMultipleElements(){
+        int maxNumberOfElementAdded = 10;
+        for(int numberOfElementAdded = 0; numberOfElementAdded < maxNumberOfElementAdded; numberOfElementAdded++){
+            circularList.add(0);
+        }
+        assertAll(
+            ()->assertEquals(maxNumberOfElementAdded, circularList.size()),
+            ()->assertFalse(circularList.isEmpty())
+        );
+    }
+
+
+    @Test
+    void verifiedListForwardIteration(){
+        int maxNumberOfElementAdded = 5;
+        for(int numberOfElementAdded = 0; numberOfElementAdded < maxNumberOfElementAdded; numberOfElementAdded++){
+            circularList.add(numberOfElementAdded);
+        }
+        
+        for(int numberOfElementAdded = 0; numberOfElementAdded < maxNumberOfElementAdded-1; numberOfElementAdded++){
+            circularList.next();
+        }
+        Optional<Integer> lasrElement = circularList.next();
+
+        assertAll(
+            ()-> assertTrue(lasrElement.isPresent()),
+            ()-> assertEquals(maxNumberOfElementAdded-1, lasrElement.get())
+        );
+    }
+
+    @Test
+    void verifiedListCircolarForwardIteration(){
+        int maxNumberOfElementAdded = 5;
+        for(int numberOfElementAdded = 0; numberOfElementAdded < maxNumberOfElementAdded; numberOfElementAdded++){
+            circularList.add(numberOfElementAdded);
+        }
+        
+        for(int numberOfElementAdded = 0; numberOfElementAdded < (maxNumberOfElementAdded * 2) - 1; numberOfElementAdded++){
+            circularList.next();
+        }
+        Optional<Integer> lasrElement = circularList.next();
+
+        assertAll(
+            ()-> assertTrue(lasrElement.isPresent()),
+            ()-> assertEquals(maxNumberOfElementAdded-1, lasrElement.get())
+        );
+        
+    }
+
+    @Test
+    void verifiedListBackIteration(){
+        int maxNumberOfElementAdded = 5;
+        for(int numberOfElementAdded = 0; numberOfElementAdded < maxNumberOfElementAdded; numberOfElementAdded++){
+            circularList.add(numberOfElementAdded);
+        }
+        
+        for(int numberOfElementAdded = 0; numberOfElementAdded < maxNumberOfElementAdded-1; numberOfElementAdded++){
+            circularList.previous();
+        }
+        Optional<Integer> lasrElement = circularList.previous();
+
+        assertAll(
+            ()-> assertTrue(lasrElement.isPresent()),
+            ()-> assertEquals(0, lasrElement.get())
+        );
+    }
+
+    @Test
+    void listEmptyAfterReset(){
+        int maxNumberOfElementAdded = 5;
+        for(int numberOfElementAdded = 0; numberOfElementAdded < maxNumberOfElementAdded; numberOfElementAdded++){
+            circularList.add(numberOfElementAdded);
+        }
+        assertEquals(5, circularList.size());
+
+        circularList.reset();
+
+        assertTrue(circularList.isEmpty());
+    }
+
 
 
 
